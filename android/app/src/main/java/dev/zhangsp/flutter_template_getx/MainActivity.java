2bfbadbe88c30733b5b4a2af98476d9a912479f9
张仕鹏 <1120148291@qq.com>
2025-02-14 14:22:41 +0800

    项目初始化模板搭建

---
+++ android/app/src/main/java/dev/zhangsp/flutter_template_getx/MainActivity.java
@@ -0,0 +1,6 @@
+package dev.zhangsp.flutter_template_getx;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(
+    MyApp()
+  );
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    );
+  }
+}
