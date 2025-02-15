831847638ba1653ce5c59f7acab9a4e8b1631733
zhangshipeng <zhangshipeng@zhangshipengdeMac-mini.local>
2025-02-15 22:31:02 +0800

    多环境配置完成并更新GetMaterialApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,44 +1,25 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_template_getx/app/bingdings/initial_binding.dart';
+import 'package:flutter_template_getx/app/routes/app_pages.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'flavors.dart';
-import 'pages/my_home_page.dart';
 
 class App extends StatelessWidget {
-
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: F.title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: _flavorBanner(
-        child: MyHomePage(),
-        show: kDebugMode,
-      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppPages.INITIAL,
+      initialBinding: InitialBinding(),
+      defaultTransition: Transition.rightToLeft,
+      transitionDuration: const Duration(milliseconds: 200),
+      getPages: AppPages.routes,
     );
   }
-
-  Widget _flavorBanner({
-    required Widget child,
-    bool show = true,
-  }) =>
-      show
-          ? Banner(
-        child: child,
-        location: BannerLocation.topStart,
-        message: F.name,
-        color: Colors.green.withOpacity(0.6),
-        textStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 12.0,
-            letterSpacing: 1.0),
-        textDirection: TextDirection.ltr,
-      )
-          : Container(
-        child: child,
-      );
 }
